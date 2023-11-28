fc84fae7ca2c4688932b1080aa474e912dc76aa4
object1037 <contact@object1037.dev>
2023-11-28 16:30:17 +0900

    calculate RGB value from sensor inputs

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,47 +1,51 @@
 #include <Adafruit_MMC56x3.h>
 
-const int TRIG_1 = 16;
-const int ECHO_1 = 10;
-const int TRIG_2 = 9;
-const int ECHO_2 = 8;
+const int TRIG_H = 16;
+const int ECHO_H = 10;
+const int TRIG_V = 9;
+const int ECHO_V = 8;
 
-const int dist1_min = 3;
-const int dist1_max = 40;
-const int dist2_min = 3;
-const int dist2_max = 40;
+const int DIST_H_MIN = 15;
+const int DIST_H_MAX = 27;
+const int DIST_V_MIN = 18;
+const int DIST_V_MAX = 43;
 
 typedef struct {
-  float L;
-  float C;
-  float h;
+  double L;
+  double C;
+  double h;
 } LCh_t;
 typedef struct {
-  float r;
-  float g;
-  float b;
+  double r;
+  double g;
+  double b;
 } RGB_t;
+
+const double hard_iron[3] = {32.51, -21.84, 54.53};
+const double soft_iron[3][3] = {
+  {1.006, 0.018, 0.001},
+  {0.018, 0.956, -0.009},
+  {0.001, 0.009, 1.041}
+};
 
 Adafruit_MMC5603 mag = Adafruit_MMC5603(12345);
 sensors_event_t mag_event;
 
-float min_x, max_x, mid_x;
-float min_y, max_y, mid_y;
-
-double dist1_lp = 0;
-double dist2_lp = 0;
+double dist_h_lp = 0;
+double dist_v_lp = 0;
 
 double lowpass(double sample, double prev, double rc) {
   return sample - rc * (sample - prev);
 }
 
-double measure_distance(int TRIG, int ECHO, float temp) {
+double measure_distance(int TRIG, int ECHO, double temp) {
   digitalWrite(TRIG, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
   double duration = pulseIn(ECHO, HIGH);
-  float velocity = 331.45 + 0.61 * temp;
+  double velocity = 331.45 + 0.61 * temp;
 
   if (duration > 0) {
     double distance = duration / 2 / 1000000 * velocity * 100;
@@ -50,51 +54,63 @@ double measure_distance(int TRIG, int ECHO, float temp) {
   return -1;
 }
 
-void calibrate_mag(sensors_event_t* mag_event, float* x, float* y) {
-  float raw_x = mag_event->magnetic.x;
-  float raw_y = mag_event->magnetic.y;
+double dist_percentage(double dist, int d_min, int d_max) {
+  int range = d_max - d_min;
+  return (dist - d_min) / range;
+}
 
-  min_x = min(min_x, raw_x);
-  min_y = min(min_y, raw_y);
+void calibrate_mag(sensors_event_t* mag_event, double* x, double* y) {
+  double mag_data[] = {
+    mag_event->magnetic.x,
+    mag_event->magnetic.y,
+    mag_event->magnetic.z
+  };
+  double hi_cal[3];
 
-  max_x = max(max_x, raw_x);
-  max_y = max(max_y, raw_y);
+  for (int i = 0; i < 3; i++) {
+    hi_cal[i] = mag_data[i] - hard_iron[i];
+  }
+  for (int i = 0; i < 3; i++) {
+    mag_data[i] = soft_iron[i][0] * hi_cal[0] + soft_iron[i][1] * hi_cal[1] + soft_iron[i][2] * hi_cal[2];
+  }
 
-  mid_x = (max_x + min_x) / 2;
-  mid_y = (max_y + min_y) / 2;
-
-  *x = raw_x - mid_x;
-  *y = raw_y - mid_y;
+  *x = mag_data[0];
+  *y = mag_data[1];
 
   return;
 }
 
-float get_heading(const float x, const float y) {
-  float heading = -1 * atan2(x, y);
+double get_heading(const double x, const double y) {
+  double heading = -1 * atan2(x, y);
   if (heading < 0) {
     heading += 2 * M_PI;
   }
   return heading;
 }
 
-float gamma_correction(float c) {
-  if (c <= 0.0031308) {
+double gamma_correction(double c) {
+  double abs = abs(c);
+  int sign = 1;
+  if (c < 0) {
+    sign = -1;
+  }
+  if (abs <= 0.0031308) {
     return c * 12.92;
   }
-  return 1.055 * pow(c, 1 / 2.4) - 0.055;
+  return sign * (1.055 * pow(abs, 1.0 / 2.4) - 0.055);
 }
 
 RGB_t oklch_to_rgb(LCh_t* lch) {
-  float a = lch->C * cos(lch->h);
-  float b = lch->C * sin(lch->h);
+  double a = lch->C * cos(lch->h);
+  double b = lch->C * sin(lch->h);
 
-  float l_ = lch->L + 0.3963377774f * a + 0.2158037573f * b;
-  float m_ = lch->L - 0.1055613458f * a - 0.0638541728f * b;
-  float s_ = lch->L - 0.0894841775f * a - 1.2914855480f * b;
+  double l_ = lch->L + 0.3963377774f * a + 0.2158037573f * b;
+  double m_ = lch->L - 0.1055613458f * a - 0.0638541728f * b;
+  double s_ = lch->L - 0.0894841775f * a - 1.2914855480f * b;
 
-  float l = l_*l_*l_;
-  float m = m_*m_*m_;
-  float s = s_*s_*s_;
+  double l = l_*l_*l_;
+  double m = m_*m_*m_;
+  double s = s_*s_*s_;
 
   return {
     .r = gamma_correction(+4.0767416621f * l - 3.3077115913f * m + 0.2309699292f * s),
@@ -106,10 +122,10 @@ RGB_t oklch_to_rgb(LCh_t* lch) {
 void setup() {
   Serial.begin(9600);
 
-  pinMode(TRIG_1, OUTPUT);
-  pinMode(TRIG_2, OUTPUT);
-  pinMode(ECHO_1, INPUT);
-  pinMode(ECHO_2, INPUT);
+  pinMode(TRIG_H, OUTPUT);
+  pinMode(TRIG_V, OUTPUT);
+  pinMode(ECHO_H, INPUT);
+  pinMode(ECHO_V, INPUT);
 
   if (!mag.begin(MMC56X3_DEFAULT_ADDRESS, &Wire)) {
     Serial.println(F("Could not find a magnetometer, check wiring!"));
@@ -118,37 +134,52 @@ void setup() {
   delay(100);
 
   mag.getEvent(&mag_event);
-  min_x = max_x = mag_event.magnetic.x;
-  min_y = max_y = mag_event.magnetic.y;
   
   delay(10);
 }
 
 void loop() {
   mag.getEvent(&mag_event);
-  float mag_x = 0;
-  float mag_y = 0;
+  double mag_x = 0;
+  double mag_y = 0;
   calibrate_mag(&mag_event, &mag_x, &mag_y);
 
-  float heading_rad = get_heading(mag_x, mag_y);
-  float heading_deg = heading_rad * 180 / M_PI;
+  double heading_rad = get_heading(mag_x, mag_y);
+  double heading_deg = heading_rad * 180 / M_PI;
 
-  float temp = mag.readTemperature();
-  double dist1_raw = measure_distance(TRIG_1, ECHO_1, temp);
-  double dist2_raw = measure_distance(TRIG_2, ECHO_2, temp);
-  dist1_lp = lowpass(constrain(dist1_raw, dist1_min, dist1_max), dist1_lp, 0.5);
-  dist2_lp = lowpass(constrain(dist2_raw, dist2_min, dist2_max), dist2_lp, 0.5);
+  double temp = mag.readTemperature();
+  double dist_h_raw = measure_distance(TRIG_H, ECHO_H, temp);
+  double dist_v_raw = measure_distance(TRIG_V, ECHO_V, temp);
+  dist_h_lp = lowpass(constrain(dist_h_raw, DIST_H_MIN, DIST_H_MAX), dist_h_lp, 0.5);
+  dist_v_lp = lowpass(constrain(dist_v_raw, DIST_V_MIN, DIST_V_MAX), dist_v_lp, 0.5);
+  double h_perc = dist_percentage(dist_h_lp, DIST_H_MIN, DIST_H_MAX);
+  double v_perc = dist_percentage(dist_v_lp, DIST_V_MIN, DIST_V_MAX);
+
+  LCh_t lch = {
+    .L = v_perc,
+    .C = 0.3 * h_perc,
+    .h = heading_rad
+  };
+  RGB_t rgb = oklch_to_rgb(&lch);
 
   // Serial.print(F("Heading: "));
   Serial.print(heading_deg); Serial.print(", ");
-  Serial.print((max_x - min_x)/2); Serial.print(", ");
-  Serial.print((max_y - min_y)/2); Serial.print(", ");
 
   // Serial.print(F(" Dist: "));
-  Serial.print(dist1_raw); Serial.print(", ");
-  Serial.print(dist1_lp); Serial.print(", ");
-  Serial.print(dist2_raw); Serial.print(", ");
-  Serial.print(dist2_lp);
+  Serial.print(dist_h_lp); Serial.print(", ");
+  Serial.print(h_perc); Serial.print(", ");
+  Serial.print(dist_v_lp); Serial.print(", ");
+  Serial.print(v_perc); Serial.print(", ");
+
+  Serial.print("lch(");
+  Serial.print(lch.L, 5); Serial.print(", ");
+  Serial.print(lch.C, 5); Serial.print(", ");
+  Serial.print(lch.h * 180 / M_PI); Serial.print(") -> ");
+
+  Serial.print("rgb(");
+  Serial.print(255 * rgb.r); Serial.print(", ");
+  Serial.print(255 * rgb.g); Serial.print(", ");
+  Serial.print(255 * rgb.b); Serial.print(")");
 
   Serial.println();
 
